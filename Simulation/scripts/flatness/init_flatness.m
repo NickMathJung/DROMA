22a36548c7c0727069b6b63119e85e346b4e32a3
Nick <nickfjung@gmail.com>
2026-08-01 14:47:51 +0200

    flatness: eigenvalPos 8/10 + gamma_khat=0.05 -> 4.3mm RMS (z-Ripple gefixt)

--- Simulation/scripts/flatness/init_flatness.m
+++ Simulation/scripts/flatness/init_flatness.m
@@ -12,9 +12,9 @@ end
 % --- Brunovsky-Polvorgabe der flachen Fehlerdynamik ---------------------------
 % Je Achse ein Satz reeller Pole [rad/s] fuer die Integratorkette (Pos bis Snap).
 % z (Zeile 3) schneller gewaehlt (wie in der Kaskade omega_pos_z > x,y).
-eigenvalPos = [ 2.0  2.0  2.0  2.0;     % x
-                1.5  2.0  2.0  2.0;     % y
-                4.0  4.0  4.0  4.0 ];   % z
+eigenvalPos = [ 8.0   8.0   8.0   8.0;     % x
+                8.0   8.0   8.0   8.0;      % y
+                10.0  10.0  10.0  10.0 ];   % z
 coefPos = zeros(3,5);
 for jr = 1:3
     cp = [1 eigenvalPos(jr,1)];
@@ -32,7 +32,9 @@ fctrl.coefPos = coefPos;                 % 3x5  -> flatness_ctrl
 fctrl.coefPhi = coefPhi;                 % 1x3  -> flatness_ctrl
 
 % --- Schub-Skalenschaetzer (flatness_khat) ------------------------------------
-fctrl.gamma_khat = 0.5;                  % Adaptionsgain (validiert)
+fctrl.gamma_khat = 0.05;                 % Adaptionsgain: langsam (tau~2s) -> faengt
+                                         % quasistat. Download, ohne Manoever-z-Ripple
+                                         % anzufachen. Sim: z-max 3.9->1.6cm, RMS ->4.3mm.
 fctrl.tau_lp     = 0.10;                 % TP auf differenzierte Beobachter-v [s]
 fctrl.k_hat0     = 1.0;                  % Startwert
 
