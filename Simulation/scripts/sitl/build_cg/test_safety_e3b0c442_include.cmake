a07428a7d586df52ad59e8b520fc6828f8e196a2
Nick <nickfjung@gmail.com>
2026-08-02 14:39:34 +0200

    chore: build_cg/ enttracken (maschinenspezifische CMake-Artefakte, wie build/)

--- Simulation/scripts/sitl/build_cg/test_safety_e3b0c442_include.cmake
+++
@@ -1,3 +0,0 @@
-if(EXISTS "C:/Users/Rakete/Documents/Drohnenversuchsstand/DROMA/Simulation/scripts/sitl/build_cg/test_safety_e3b0c442_${CTEST_CONFIGURATION_TYPE}_include.cmake")
-  include("C:/Users/Rakete/Documents/Drohnenversuchsstand/DROMA/Simulation/scripts/sitl/build_cg/test_safety_e3b0c442_${CTEST_CONFIGURATION_TYPE}_include.cmake")
-endif()
